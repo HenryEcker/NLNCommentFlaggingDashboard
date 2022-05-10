40bea05f6ea618b81b27e1999dabf642aed0ec31
HenryEcker <hde1997@gmail.com>
2022-05-10 17:48:13 -0400

    Updated bro blacklist

--- src/GlobalVars.ts
+++ src/GlobalVars.ts
@@ -22,11 +22,11 @@ export const blacklist = mergeRegexes([
     /(?:happy\s+coding)/,
     // This solved my issue/This resolved my issue/This fixed my issue
     /(it(['’]?s)?|this)\s*((re)?solved?|fix(ed)?)\s*(((m[ey]|the)\s*(issue|problem))|it)/,
-    // TRE('bro', 'dude', 'man', 'bud', 'buddy', 'amigo', 'pal', 'homie', 'friend', 'friendio', 'friendo', 'mate', 'sir', 'fam')
-    // bro often an unknown number of o's so should be bro+
+    // TRE('broo', 'dude', 'man', 'bud', 'buddy', 'amigo', 'pal', 'homie', 'friend', 'friendio', 'friendo', 'mate', 'sir', 'fam', 'brother')
+    // bro often an unknown number of o's so should be bro+ (broo* so it can be added with brother)
     // man often has an unknown number of n's so it should be man+
     // (needs manually re-added whenever the TRE is re-built)
-    /\b(?:f(?:riend(?:io|o)?|am)|b(?:ud(?:dy)?|ro+)|ma(?:te|n+)|amigo|homie|dude|pal|sir)\b/,
+    /\b(?:b(?:ro(?:ther|o*)|ud(?:dy)?)|f(?:riend(?:io|o)?|am)|ma(?:te|n+)|amigo|homie|dude|pal|sir)\b/,
     /*
      * Following rules modified from https://github.com/kamil-tekiela/commentBot/blob/master/src/Comment.php
      */
