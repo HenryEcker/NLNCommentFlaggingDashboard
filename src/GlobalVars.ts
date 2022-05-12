761472fd5bb5c1b16ac8451f97a3235f6c3ebcbd
HenryEcker <hde1997@gmail.com>
2022-05-12 17:51:32 -0400

    blacklist tweaks

--- src/GlobalVars.ts
+++ src/GlobalVars.ts
@@ -2,8 +2,6 @@ import {mergeRegexes} from "./Utils";
 
 
 export const blacklist = mergeRegexes([
-    // @ mentions
-    /@\w+/,
     // Emojis
     /(?:[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/,
     // Ascii Smileys/Punctuation spam
