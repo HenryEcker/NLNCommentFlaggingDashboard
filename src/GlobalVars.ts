e1bcb47f070e42489ca4395a27a0b1cd3f94b366
HenryEcker <hde1997@gmail.com>
2022-06-13 10:40:25 -0400

    Blacklist tweaks

--- src/GlobalVars.ts
+++ src/GlobalVars.ts
@@ -5,7 +5,7 @@ export const blacklist = mergeRegexes([
     // Emojis
     /(?:[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/,
     // Ascii Smileys/Punctuation spam
-    /\s+((?=[!-~])[\W_]){2,}\s*/,
+    /\b\s+((?=[!-~])[\W_]){2,}\s*\b/,
     // Text-speak
     /*
 from trieregex import TrieRegEx as TRE
@@ -20,21 +20,21 @@ TRE(
      */
     /\b(?:t(?:y(?:sm|vm)?|hx)|lm(?:fao|ao)|ily(?:sm)?|om(?:fg|g)|k)\b/,
     // Glad to help/Happy I could help/Glad to hear
-    /(?:happy|glad)\s*(?:\w+\s+)*?(he(?:ar|lp))/,
+    /\b(?:happy|glad)\s*(?:\w+\s+)*?(he(?:ar|lp))\b/,
     // You're/that's awesome!
-    /(?:you(r|['’]?re|\s+are)?|that['’]?s?)\s+(?:(a\s+)?(\s+rock\s+star|amazing|awesome|incredible|brilliant|wonderful|rock|perfect|genius))[.!]?/,
+    /\b(?:you(r|['’]?re|\s+are)?|that['’]?s?)\s+(?:(a\s+)?(\s+rock\s+star|amazing|awesome|incredible|brilliant|wonderful|rock|perfect|genius))\b(?:[.!]*)/,
     // OMG
-    /(oh\s+)?(my\s+)?(god|goodness)[.!]?|(holy\s+\w+)/,
+    /\b(oh\s+)?(my\s+)?(god|goodness)|(holy\s+\w+)\b(?:[.!])*/,
     // Any help would be appreciated
-    /(?:Any\s+help\s+would\s+be\s+(?:a(?:ppreciated|wesome)|wonderful|great))/,
+    /\b(?:Any\s+help\s+would\s+be\s+(?:a(?:ppreciated|wesome)|wonderful|great))\b/,
     // was what I needed/was what I was looking for/that's it
-    /(?:(this|that|it)?(((['’]s?)|\s+((wa|i)s))\s+)(it|(what\s+(\w+\s+)*?(need(?:ed|ing)?|looking\s*for)))[.!]?)/,
+    /\b(?:(this|that|it)?(((['’]s?)|\s+((wa|i)s))\s+)(it|(what\s+(\w+\s+)*?(need(?:ed|ing)?|looking\s*for))))\b(?:[.!]*)/,
     // Happy coding
-    /(?:happy\s+coding)/,
+    /\b(?:happy\s+coding)\b/,
     // Have a great day
-    /(have a\s+(\w+\s+)*?(day|evening|night)([.!]*)?)/,
+    /\b(have a\s+(\w+\s+)*?(day|evening|night))\b(?:[.!]*)/,
     // This solved my issue/This resolved my issue/This fixed my issue
-    /(it(['’]?s)?|this)\s*((re)?solved?|fix(ed)?)\s*(((m[ey]|the)\s*(issue|problem))|it)/,
+    /\b(it(['’]?s)?|this)\s*((re)?solved?|fix(ed)?)\s*(((m[ey]|the)\s*(issue|problem))|it)\b/,
     /*
 from trieregex import TrieRegEx as TRE
 
@@ -47,30 +47,31 @@ TRE('broo', 'boss', 'dude', 'man', 'bud', 'buddy', 'amigo', 'pal', 'homie',
     // (needs manually re-added whenever the TRE is re-built)
     /\b(?:b(?:ro(?:ther|o*)|ud(?:dy)?|oss)|f(?:riend(?:io|o)?|ella|am)|s(?:oldier|enpai|ir)|d(?:addy|ude)|ma(?:te|n+)|amigo|champ|homie|pal)\b/,
     // thanks
-    /(?:(?:big\s+|many\s+)?th?ank(?:s|\s*you|\s*u)?(?:\s+a lot|\s+(?:very|so) much|\s+a mil+ion|\s+)?(?:\s*for (?:(your|the)\s+)?(?:help(ing)?)?)?|th?anx|thx|cheers)/,
+    /\b(?:(?:big\s+|many\s+)?th?ank(?:s|\s*you|\s*u)?(?:\s+a lot|\s+(?:very|so) much|\s+a mil+ion|\s+)?(?:\s*for (?:(your|the)\s+)?(?:help(ing)?)?)?|th?anx|thx|cheers)\b/,
     // Your [very excellent] solution [also] worked like a charm
-    /((this|that|it(['’]?s)?|your)\s+)?(?:(excellent|wonderful|awesome)\s+)?((solution|answer|code)\s+)?(((doe|i)s|also)\s+)?work(?:ed|s|ing)?(?:\s+(now|perfectly|great|for\s+me|((like|as)\s+)?(a\s+)?charm|again|[!.]))?/,
+    /\b((this|that|it(['’]?s)?|your)\s+)?(?:(excellent|wonderful|awesome)\s+)?((solution|answer|code)\s+)?(((doe|i)s|also)\s+)?work(?:ed|s|ing)?(?:\s+(now|perfectly|great|for\s+me|((like|as)\s+)?(a\s+)?charm|again))?\b(?:[!.]*)/,
     // you are welcome/my pleasure
-    /(?:(?:you(?:['’]?re?|\s+are)\s+)?welcome|my pleasure)+/,
+    /\b(?:(?:you(?:['’]?re?|\s+are)\s+)?welcome|my pleasure)+\b/,
     // this was very helpful
-    /(?:(?:I\s+)?(?:hope\s+)?(?:your\s+|(?:this\s+|that\s+|it\s+)(?:was\s+|is\s+)?)?(?:very\s+)?help(?:ful|ed|s)|useful(?:\s+a lot|\s+(?:very|so) much)?)+/,
+    /\b(?:(?:I\s+)?(?:hope\s+)?(?:your\s+|(?:this\s+|that\s+|it\s+)(?:was\s+|is\s+)?)?(?:very\s+)?help(?:ful|ed|s)|useful(?:\s+a lot|\s+(?:very|so) much)?)+\b/,
     // TRE('ingenious', 'superb', 'amazing', 'fantastic', 'perfect', 'wonderful', 'brilliant', 'excellent', 'marvelous', 'awesome', 'bravo')
     // wow has any number of wow so should be w+o+w+ (should be added at the end of the OR list)
-    /(?:a(?:mazing|wesome)|br(?:illiant|avo)|excellent|fantastic|ingenious|marvelous|wonderful|perfect|superb|w+o+w+)/,
+    /\b(?:a(?:mazing|wesome)|br(?:illiant|avo)|excellent|fantastic|ingenious|marvelous|wonderful|perfect|superb|w+o+w+)\b/,
     // saviour
-    /(?:you(['’]?re?|\s+are|['’]?ve|\s+have)?|this(\s+(wa|i|['’])s)?\s+)?(?:a\s+life(-|\s+)saver|sav(e|ed|ing)\s+(m[ey]|the|it))/,
+    /\b(?:(you(['’]?re?|\s+are|['’]?ve|\s+have)?|this(\s+(wa|i|['’])s)?)\s+)?(?:a\s+life(-|\s+)saver|sav(e|ed|ing)\s+(m[ey]|the|it))(\s+\w+)?\b/,
     // please accept
-    /(?:please(?:\s+\w+)*\s+)?accept(?:ed|ing)?\b(?:\s+the\s+answer)?/,
+    /\b(?:please(?:\s+\w+)*\s+)?accept(?:ed|ing)?\b(?:\s+the\s+answer)?\b/,
     // hit the checkmark
-    /(click(ing)?|hit(t?ing)?)\s+(the\s+)?(gr[ae](en|y)\s+)?check(mark)?(\s+to\s+the\s+left|(((?:\s+\w+)*\s+)?answer))?/,
+    /\b(click(ing)?|hit(t?ing)?)\s+(the\s+)?(gr[ae](en|y)\s+)?check(mark)?(\s+to\s+the\s+left|(((?:\s+\w+)*\s+)?answer))?\b/,
     // mark this answer as correct
-    /(?:please(?:\s+\w+)*\s+)?mark\s+th(?:e|is)(\s+answer)?((\s+as)?(\s+the)?(\s+(correct|right))?(\s+answer)?)?/,
+    /\b(?:please(?:\s+\w+)*\s+)?mark\s+th(?:e|is)(\s+answer)?((\s+as)?(\s+the)?(\s+(correct|right))?(\s+answer)?)?\b/,
     // please upvote
-    /(?:please(?:\s+\w+)*\s+)?(?:give an?\s+)?upvot(?:ed?|ing)(?:\s+the answer)?/,
+    /\b(?:please(?:\s+\w+)*\s+)?(?:give an?\s+)?upvot(?:ed?|ing)(?:\s+the answer)?\b/,
     // is/should be the correct/accepted/right answer
-    /(?:is|should be)(?:\s+\w+)*\s+(?:right|correct|accepted)(?:\s+\w+)*\s+(?:answer|solution)/,
+    /\b(?:is|should be)(?:\s+\w+)*\s+(?:right|correct|accepted)(?:\s+\w+)*\s+(?:answer|solution)\b/,
     // /help/someone-answer or /help/accepted-answer
-    /(?:(https:\/\/stackoverflow.com)?\/help\/(someone-answers|accepted-answer))/
+    /\b(?:(https:\/\/stackoverflow.com)?\/help\/(someone-answers|accepted-answer))\b/,
+    /\b(?:b+u+m+p+)\b(?:[!.?~@#$^%]*)/
 ], 'gi');
 
 export const whitelist = mergeRegexes([
@@ -91,26 +92,6 @@ suffixes = ["n't", "n’t", "n'", "n’", "nt"]
 TRE(*[f'{b}{s}' for b, s in product(bases, suffixes)]).regex()
      */
     /\b(?:d(?:o(?:esn(?:'t?|’t?|t)|n(?:'t?|’t?|t))|idn(?:'t?|’t?|t))|c(?:ouldn(?:'t?|’t?|t)|an(?:'t?|’t?|t))|ha(?:ven(?:'t?|’t?|t)|sn(?:'t?|’t?|t))|wo(?:uldn(?:'t?|’t?|t)|n(?:'t?|’t?|t))|a(?:ren(?:'t?|’t?|t)|in(?:'t?|’t?|t))|shouldn(?:'t?|’t?|t)|isn(?:'t?|’t?|t))\b/,
-    /\b(will|I'?ll)\s*try\b/,
+    /\b(will|I['’]?ll)\s*try\b/,
     /[?]/
 ], 'gi');
-
-
-/**
- * Base Class of Errors which share the name with their class name
- */
-class SelfNamedError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = this.constructor.name;
-    }
-}
-
-/**
- * Errors used to differentiate the various failure modes
- */
-export class FlagAttemptFailed extends SelfNamedError {
-}
-
-export class RatedLimitedError extends SelfNamedError {
-}
