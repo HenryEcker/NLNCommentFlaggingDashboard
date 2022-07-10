b34364e7ac59675553d6dabe70f39508a7e5a3b9
HenryEcker <hde1997@gmail.com>
2022-07-10 16:04:35 -0400

    Bound modal content width to match #content

--- src/UI/Dashboard/DashboardCommentTable.module.scss.d.ts
+++ src/UI/Dashboard/DashboardCommentTable.module.scss.d.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 export type Styles = {
-    'modal-body': string;
-    'modal-close-button': string;
-    'modal-content': string;
-    'modal-dashboard-wrapper': string;
-    'modal-header': string;
-    'modal-header-text': string;
+  'modal-body': string;
+  'modal-close-button': string;
+  'modal-content': string;
+  'modal-dashboard-wrapper': string;
+  'modal-header': string;
+  'modal-header-text': string;
 };
 
 export type ClassNames = keyof Styles;
